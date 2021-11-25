cea0b06098397bdc0bdaa50af65ce7df5ec260cd
Ruan Rodrigues <rupriwell123@gmail.com>
2021-11-25 08:39:44 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -17,33 +17,27 @@ const DonutChart = () => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
         
             const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sallerName);
+            const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
     
             setChartData({labels: myLabels, series: mySeries});
         });
     }, []);
     
-       //const mockData = {
-       // series: [477138, 499928, 444867, 220426, 473088],
-        //labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-       //}
-    
-     const options = {
+    const options = {
         legend: {
             show: true
         }
     }
-   
-     return (
-       <Chart 
-       options={{...options,labels:chartData.labels}}
-       series={chartData.series}
-       type="donut"
-       height="240"
-       />
+
+    return (
+        <Chart 
+            options={{ ...options, labels: chartData.labels}} 
+            series={chartData.series} 
+            type="donut" 
+            height="240"
+        />
     );
   }
   
   export default DonutChart;
-  
