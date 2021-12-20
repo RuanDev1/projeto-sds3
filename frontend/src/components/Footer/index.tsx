5593d0a1ba884a53ac7e42dd953e7d7e599b15e4
Ruan Rodrigues <39811287+ruangja@users.noreply.github.com>
2021-12-20 18:53:09 -0300

    Update index.tsx

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
         <div className="container">
-          <p className="text-light">App desenvolvido por <a href="https://github.com/ruangja/projeto-sds3'" target="_blank" rel="noreferrer">Ruan Rodrigues</a></p>
+          <p className="text-light">App desenvolvido por <a href="https://github.com/ruangja/projeto-sds3" target="_blank" rel="noreferrer">Ruan Rodrigues</a></p>
           <p className="text-light"><small><strong>Semana Spring React</strong><br/>
             Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
         </div>
